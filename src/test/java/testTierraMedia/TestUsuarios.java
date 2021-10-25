e14a6a5ac395874527f5e7c0d710a63895d5296a
normins <62967178+normins@users.noreply.github.com>
2021-10-25 07:32:29 -0300

    Corresponde al test de Usuarios

--- src/test/java/testTierraMedia/TestUsuarios.java
+++ src/test/java/testTierraMedia/TestUsuarios.java
@@ -1,170 +1,265 @@
 package testTierraMedia;
 
 import static org.junit.Assert.*;
-import org.junit.Before;
+
+import java.sql.SQLException;
+
 import org.junit.Test;
 
-import model.Atraccion;
-import model.PromoAbsoluta;
-import model.Promocion;
+import dao.AtraccionDAO;
+import dao.DAOFactory;
+import dao.UsuarioDAO;
+import model.Ofertable;
 import model.Usuario;
-;
-
 
 public class TestUsuarios {
 
-	Usuario u1;
+	UsuarioDAO usuarioDAO = DAOFactory.getUsuarioDAO();
 
-	@Before
-	public void iniciar() {
-		u1 = new Usuario("spider",1000, 100, "paisaje");
+	@Test
+	public void queSeCreeUsuarioYPoderRecuperarlo() throws SQLException {
+		Usuario galadriel = new Usuario("Galadriel", 100, 33, "Aventura");
+		usuarioDAO.insertar(galadriel);
+		assertNotNull(galadriel);
+		// Restaurar
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("Galadriel");
+		assertNotNull(galadrielRecuperado);
+		usuarioDAO.borrar(galadrielRecuperado);
 	}
 
 	@Test
-	public void queSeCreanUsuarios() {
-		assertNotNull(u1);
+	public void queElNombreDelUsuarioSeaIgualAlIngresado() throws SQLException {
+		Usuario galadriel = new Usuario("Galadriel", 100, 33, "Aventura");
+		usuarioDAO.insertar(galadriel);
+		assertEquals("Galadriel", galadriel.getNombre());
+		// Restaurar
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("Galadriel");
+		assertNotNull(galadrielRecuperado);
+		usuarioDAO.borrar(galadrielRecuperado);
 	}
 
-	
-	@Test (expected = Error.class)
-	public void queNoSeCreanUsuariosConConDineroNegativo() {
-		u1 = new Usuario("spider",-1000, 100, "paisaje");
-	}
-	
-	@Test (expected = Error.class)
-	public void queNoSeCreanUsuariosConConTiempoNegativo() {
-		u1 = new Usuario("spider",1000, -100, "paisaje");
-	}
-	
-	
 	@Test
-	public void queGastaDinero() {
-		double dinero = u1.getDineroDisponible();
-		assertEquals(1000, dinero, 0.0);
-		u1.gastarDinero(900);
-		assertEquals(100, u1.getDineroDisponible(), 0.0);
+	public void queLosCamposRegistradosSeanIgualesALosIngresados() throws SQLException {
+		Usuario galadriel = new Usuario("Galadriel", 100, 33, "Aventura");
+		usuarioDAO.insertar(galadriel);
+		assertEquals("Galadriel", galadriel.getNombre());
+		assertEquals(100, galadriel.getDineroDisponible(), 0);
+		assertEquals(33, galadriel.getTiempoDisponible(), 0);
+		assertEquals("Aventura", galadriel.getTipoPreferido());
+		// Restaurar
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("Galadriel");
+		assertNotNull(galadrielRecuperado);
+		usuarioDAO.borrar(galadrielRecuperado);
 	}
 
 	@Test(expected = Error.class)
-	public void queNoSePuedeGastarMasDineroDelQueSeTiene() {
-		u1.gastarDinero(1001);
-
+	public void queNoPermitaCrearUsuarioConTiempoDisponibleNegativo() throws SQLException {
+		Usuario galadriel = new Usuario("Galadriel", -100, 33, "Aventura");
+		usuarioDAO.insertar(galadriel);
 	}
 
 	@Test(expected = Error.class)
-	public void queUsuarioNoPuedeComprarProductosSiNoLeAlcanzaElDinero() {
-
-		Atraccion impagable = new Atraccion("Mordor uno",  25000 , 10, 5, "Aventura");
-		u1.comprarProducto(impagable);
-	}
-	
-	@Test
-	public void queElUsuarioSabeCuandoUnaAtraccionLeGusta() {
-		Atraccion legusta = new Atraccion("bree",  25 , 1000000, 5, "paisaje");
-		Atraccion noLegusta = new Atraccion("Mordor",  25 , 1000000, 5, "Aventura");
-		assertFalse(u1.estaAtraccionMegusta(noLegusta));
-		assertTrue(u1.estaAtraccionMegusta(legusta));
-	}
-	
-	@Test
-	public void queCompraAtracciones() {
-
-		Atraccion atra3 = new Atraccion("Mordor tres", 25, 10, 5, "Aventura");
-		Atraccion []esperada = {atra3};
-		u1.comprarProducto(atra3);
-		assertArrayEquals(esperada,u1.getproductosComprados().toArray());	
-		
-	}
-	
-	@Test
-	public void quePagaLoQueCompra() {
-
-		Atraccion atra3 = new Atraccion("Mordor tres", 25, 10, 5, "Aventura");
-		u1.comprarProducto(atra3);
-		
-		assertEquals(975, u1.getDineroDisponible(),0);
-	}
-	
-	@Test
-	public void queRestaTiempoCuandoCompra() {
-
-		Atraccion atra3 = new Atraccion("Mordor tres", 25, 10, 5, "Aventura");
-		u1.comprarProducto(atra3);
-		
-		assertEquals(90, u1.getTiempoDisponible(),0);	
-	}
-	
-	@Test
-	public void queCompraPromociones() {
-
-		Atraccion atra = new Atraccion("Mordor uno",  25 , 10, 5, "Aventura");
-		Atraccion atra2 = new Atraccion("Mordor dos", 25, 10, 5, "Aventura");
-		Atraccion atra3 = new Atraccion("Mordor tres", 25, 10, 5, "Aventura");
-		Atraccion itinerario[] = { atra, atra2, atra3 };
-		Promocion oferta= new PromoAbsoluta("nombre",itinerario, "aventura", 20);
-		
-		Promocion []esperada = {oferta};
-		u1.comprarProducto(oferta);
-		
-		assertEquals(980, u1.getDineroDisponible(),0);
-		assertEquals(70, u1.getTiempoDisponible(),0);
-		assertArrayEquals(esperada,u1.getproductosComprados().toArray());		
-	}
-	
-	@Test
-	public void quePagaPromocionesQueCOmpra() {
-
-		Atraccion atra = new Atraccion("Mordor uno",  25 , 10, 5, "Aventura");
-		Atraccion atra2 = new Atraccion("Mordor dos", 25, 10, 5, "Aventura");
-		Atraccion atra3 = new Atraccion("Mordor tres", 25, 10, 5, "Aventura");
-		Atraccion itinerario[] = { atra, atra2, atra3 };
-		Promocion oferta= new PromoAbsoluta("nombre",itinerario, "aventura", 20);
-		
-		u1.comprarProducto(oferta);
-		assertEquals(980, u1.getDineroDisponible(),0);	
+	public void queNoSeCreanUsuariosConConTiempoNegativo() throws SQLException {
+		Usuario galadriel = new Usuario("Galadriel", 100, -33, "Aventura");
+		usuarioDAO.insertar(galadriel);
 	}
 
 	@Test
-	public void queRestaTiempoDePromoComprada() {
-
-		Atraccion atra = new Atraccion("Mordor uno",  25 , 10, 5, "Aventura");
-		Atraccion atra2 = new Atraccion("Mordor dos", 25, 10, 5, "Aventura");
-		Atraccion atra3 = new Atraccion("Mordor tres", 25, 10, 5, "Aventura");
-		Atraccion itinerario[] = { atra, atra2, atra3 };
-		Promocion oferta= new PromoAbsoluta("nombre",itinerario, "aventura", 20);
-	
-		u1.comprarProducto(oferta);
-		
-		assertEquals(70, u1.getTiempoDisponible(),0);
+	public void queAlCrearDosUsuariosSeCuentenCantidadOriginalMasDosUsuarios() throws SQLException {
+		// Cantidad original de usuarios
+		int cantUsuarios = usuarioDAO.contarTodos();
+		Usuario galadriel = new Usuario("Galadriel", 100, 33, "Aventura");
+		usuarioDAO.insertar(galadriel);
+		Usuario eowyn = new Usuario("Eowyn", 200, 66, "Paisaje");
+		usuarioDAO.insertar(eowyn);
+		// Cantidad Esperada
+		int cantUsuariosEsperada = cantUsuarios + 2;
+		int cantUsuariosObtenida = usuarioDAO.contarTodos();
+		assertEquals(cantUsuariosEsperada, cantUsuariosObtenida);
+		// Restaurar
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("Galadriel");
+		assertNotNull(galadrielRecuperado);
+		usuarioDAO.borrar(galadrielRecuperado);
+		Usuario eowynRecuperado = usuarioDAO.encontrarPorNombre("Eowyn");
+		assertNotNull(eowynRecuperado);
+		usuarioDAO.borrar(eowynRecuperado);
 	}
-	
+
 	@Test
-	public void quePagaConDineroLasAtraccionse() {
+	public void queAlCrearUnUsuarioSePuedaRecuperarPorNombre() throws SQLException {
+		Usuario galadriel = new Usuario("Galadriel", 100, 33, "Aventura");
+		usuarioDAO.insertar(galadriel);
 
-		Atraccion atra = new Atraccion("Mordor uno", 25, 10, 5, "Aventura");
-		u1.comprarProducto(atra);
-		assertEquals(975, u1.getDineroDisponible(), 0);
+		// Recuperarlo por nombre
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("Galadriel");
+		assertEquals(galadriel.getNombre(), galadrielRecuperado.getNombre());
+		assertEquals(galadriel.getDineroDisponible(), galadrielRecuperado.getDineroDisponible(), 0.01);
+		assertEquals(galadriel.getTiempoDisponible(), galadrielRecuperado.getTiempoDisponible(), 0.01);
+		assertEquals(galadriel.getTipoPreferido(), galadrielRecuperado.getTipoPreferido());
+		// Restaurar
+		usuarioDAO.borrar(galadrielRecuperado);
 	}
 
 	@Test
-	public void queCalculaElTiempo() {
+	public void queActualizaElDineroDisponible() throws SQLException {
+		// Generar usuario
+		Usuario galadriel = new Usuario("Galadriel", 100, 33, "Aventura");
+		usuarioDAO.insertar(galadriel);
+		// Recuperar desde la base de datos
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("Galadriel");
+		double dinero = galadrielRecuperado.getDineroDisponible();
+		assertEquals(100, dinero, 0.0);
+		galadrielRecuperado.gastarDinero(90);
+		assertEquals(10, galadrielRecuperado.getDineroDisponible(), 0.0);
+		// Actualizar usuario
+		usuarioDAO.actualizarDatos(galadrielRecuperado);
+		// Restaurar
+		usuarioDAO.borrar(galadrielRecuperado);
+	}
 
-		Atraccion atra = new Atraccion("Mordor uno", 25, 10, 5, "Aventura");
-		u1.comprarProducto(atra);
-		assertEquals(90, u1.getTiempoDisponible(), 0);
-	}
-	
 	@Test
-	public void queSabeCuandoUnaAtraccionYaEstaIncluidaEnUnaPromocionQueCompro() {
-		Atraccion atra = new Atraccion("Mordor uno",  25 , 10, 5, "Aventura");
-		Atraccion atra2 = new Atraccion("Mordor dos", 25, 10, 5, "Aventura");
-		Atraccion atra3 = new Atraccion("Mordor tres", 25, 10, 5, "Aventura");
-		Atraccion itinerario[] = { atra, atra2 };
-		Promocion oferta= new PromoAbsoluta("nombre",itinerario, "aventura", 20);
-	
-		u1.comprarProducto(oferta);
-		
-		assertTrue(u1.atraccionIncluidaEnPromocionComprada(atra));
-		assertFalse(u1.atraccionIncluidaEnPromocionComprada(atra3));
+	public void queActualizaElTiempoDisponible() throws SQLException {
+		// Generar usuario
+		Usuario galadriel = new Usuario("TGaladriel", 100, 10, "Aventura");
+		usuarioDAO.insertar(galadriel);
+		// Recuperar desde la base de datos
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("TGaladriel");
+		double tiempo = galadrielRecuperado.getTiempoDisponible();
+		assertEquals(10, tiempo, 0.0);
+		galadrielRecuperado.restarTiempo(3);
+		assertEquals(7, galadrielRecuperado.getTiempoDisponible(), 0.0);
+		// Actualizar usuario
+		usuarioDAO.actualizarDatos(galadrielRecuperado);
+		// Restaurar
+		usuarioDAO.borrar(galadrielRecuperado);
 	}
+
+	@Test(expected = Error.class)
+	public void queNoSePuedeGastarMasDineroDelQueSeTiene() throws SQLException {
+		// Generar usuario
+		Usuario galadriel = new Usuario("!Borrar", 100, 33, "Aventura");
+		usuarioDAO.insertar(galadriel);
+		// Recuperar desde la base de datos
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("!Borrar");
+		// Gastar de más
+		galadrielRecuperado.gastarDinero(101);
+	}
+
+	@Test(expected = Error.class)
+	public void queUsuarioNoPuedeComprarAtraccionSiNoLeAlcanzaElDinero() throws SQLException {
+		// Generar usuario
+		Usuario galadriel = new Usuario("!Borrar", 10, 33, "Aventura");
+		usuarioDAO.insertar(galadriel);
+		// Recuperar desde la base de datos
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("!Borrar");
+		// Gastar de más
+		galadrielRecuperado.gastarDinero(40);
+	}
+
+	@Test
+	public void queSeActualiceElTiempoYCostoDisponibleSegunLoQueCompra() throws SQLException {
+		// Generar usuario
+		Usuario galadriel = new Usuario("Comprador", 100, 10, "Aventura");
+		usuarioDAO.insertar(galadriel);
+		// Recuperar desde la base de datos
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("Comprador");
+
+		// Obtener atraccion desde la base de datos
+		AtraccionDAO atraccionDAO = DAOFactory.getAtraccionDAO();
+		Ofertable producto = atraccionDAO.encontrarPorID(1);
+		// atraccionDAO.encontrarPorID(1); // Moria 10 2
+
+		// Comprar atracción
+		galadrielRecuperado.comprarProducto(producto);
+		usuarioDAO.actualizarDatos(galadrielRecuperado);
+
+		// Verificar si actualiza tiempo y dinero disponible
+		Usuario galadrielModificado = usuarioDAO.encontrarPorNombre("Comprador");
+		double dineroActualizado = galadrielModificado.getDineroDisponible();
+		assertEquals(90, dineroActualizado, 0.0);
+		double tiempoActualizado = galadrielModificado.getTiempoDisponible();
+		assertEquals(8, tiempoActualizado, 0.0);
+
+		// Restaurar
+		usuarioDAO.borrar(galadrielModificado);
+	}
+
+	@Test
+	public void queElUsuarioSabeCuandoUnaAtraccionLeGusta() throws SQLException {
+		// Obtener atraccion desde la base de datos
+		AtraccionDAO atraccionDAO = DAOFactory.getAtraccionDAO();
+		Ofertable atraccionTipo1 = atraccionDAO.encontrarPorID(1);
+		Ofertable atraccionTipo2 = atraccionDAO.encontrarPorID(9);
+
+		// Generar usuario contipo de preferencia = 1
+		Usuario galadriel = new Usuario("LeGusta1", 100, 10, "Aventura");
+		usuarioDAO.insertar(galadriel);
+		// Recuperar desde la base de datos
+		Usuario galadrielRecuperado = usuarioDAO.encontrarPorNombre("LeGusta1");
+
+		assertTrue(galadrielRecuperado.estaAtraccionMegusta(atraccionTipo1));
+		assertFalse(galadrielRecuperado.estaAtraccionMegusta(atraccionTipo2));
+
+		// Restaurar
+		usuarioDAO.borrar(galadrielRecuperado);
+	}
+
+
+	/*
+	 * @Test public void queCompraPromociones() {
+	 * 
+	 * Atraccion atra = new Atraccion("Mordor uno", 25 , 10, 5, "Aventura");
+	 * Atraccion atra2 = new Atraccion("Mordor dos", 25, 10, 5, "Aventura");
+	 * Atraccion atra3 = new Atraccion("Mordor tres", 25, 10, 5, "Aventura");
+	 * Atraccion itinerario[] = { atra, atra2, atra3 }; Promocion oferta= new
+	 * PromoAbsoluta("nombre",itinerario, "aventura", 20);
+	 * 
+	 * Promocion []esperada = {oferta}; u1.comprarProducto(oferta);
+	 * 
+	 * assertEquals(980, u1.getDineroDisponible(),0); assertEquals(70,
+	 * u1.getTiempoDisponible(),0);
+	 * assertArrayEquals(esperada,u1.getproductosComprados().toArray()); }
+	 * 
+	 * @Test public void quePagaPromocionesQueCOmpra() {
+	 * 
+	 * Atraccion atra = new Atraccion("Mordor uno", 25 , 10, 5, "Aventura");
+	 * Atraccion atra2 = new Atraccion("Mordor dos", 25, 10, 5, "Aventura");
+	 * Atraccion atra3 = new Atraccion("Mordor tres", 25, 10, 5, "Aventura");
+	 * Atraccion itinerario[] = { atra, atra2, atra3 }; Promocion oferta= new
+	 * PromoAbsoluta("nombre",itinerario, "aventura", 20);
+	 * 
+	 * u1.comprarProducto(oferta); assertEquals(980, u1.getDineroDisponible(),0); }
+	 * 
+	 * @Test public void queRestaTiempoDePromoComprada() {
+	 * 
+	 * Atraccion atra = new Atraccion("Mordor uno", 25 , 10, 5, "Aventura");
+	 * Atraccion atra2 = new Atraccion("Mordor dos", 25, 10, 5, "Aventura");
+	 * Atraccion atra3 = new Atraccion("Mordor tres", 25, 10, 5, "Aventura");
+	 * Atraccion itinerario[] = { atra, atra2, atra3 }; Promocion oferta= new
+	 * PromoAbsoluta("nombre",itinerario, "aventura", 20);
+	 * 
+	 * u1.comprarProducto(oferta);
+	 * 
+	 * assertEquals(70, u1.getTiempoDisponible(),0); }
+	 * 
+	 * 
+	 * @Test public void queCalculaElTiempo() {
+	 * 
+	 * Atraccion atra = new Atraccion("Mordor uno", 25, 10, 5, "Aventura");
+	 * u1.comprarProducto(atra); assertEquals(90, u1.getTiempoDisponible(), 0); }
+	 * 
+	 * @Test public void
+	 * queSabeCuandoUnaAtraccionYaEstaIncluidaEnUnaPromocionQueCompro() { Atraccion
+	 * atra = new Atraccion("Mordor uno", 25 , 10, 5, "Aventura"); Atraccion atra2 =
+	 * new Atraccion("Mordor dos", 25, 10, 5, "Aventura"); Atraccion atra3 = new
+	 * Atraccion("Mordor tres", 25, 10, 5, "Aventura"); Atraccion itinerario[] = {
+	 * atra, atra2 }; Promocion oferta= new PromoAbsoluta("nombre",itinerario,
+	 * "aventura", 20);
+	 * 
+	 * u1.comprarProducto(oferta);
+	 * 
+	 * assertTrue(u1.atraccionIncluidaEnPromocionComprada(atra));
+	 * assertFalse(u1.atraccionIncluidaEnPromocionComprada(atra3)); }
+	 */
+
 }
